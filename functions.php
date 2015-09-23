c9d8d039cd54e8c3c4945bf40345e8fc66ddc38c
jessor <daniel@enthusiasm.cc>
2015-09-23 23:23:29 +0200

    Add icon boxes shortcode (for frontpage)

--- functions.php
+++ functions.php
@@ -16,3 +16,22 @@ function fff_enqueue_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'fff_enqueue_scripts' );
+function fff_icon_widget( $atts ) {
+    $atts = shortcode_atts( array(
+        'icon'  => 'fa-wordpress',
+        'title' => 'Text ^_^',
+        'text'  => 'Jean shorts cray whatever viral trust fund lo-fi.',
+        'href'  => '#',
+    ), $atts );
+
+    $o = '<div class="ico-box">';
+    $o .= '<span class="fa ' . esc_attr( $atts['icon'] ) . '"></span>';
+    $o .= '<p class="box-title">' . esc_attr( $atts['title'] ) . '</p>';
+    $o .= '<p>' . esc_attr( $atts['text'] ) . '</p>';
+    $o .= '</div><!-- .ico-box -->';
+
+    return $o;
+}
+
+add_shortcode( 'fff_icon_widget', 'fff_icon_widget' );
+
