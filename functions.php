e2741781e099d81b0514ac59de9a06c30e63b3cc
jessor <daniel@enthusiasm.cc>
2015-09-25 18:06:51 +0200

    Add Call to Action shortcode

--- functions.php
+++ functions.php
@@ -21,6 +21,36 @@ add_action( 'wp_enqueue_scripts', 'fff_enqueue_scripts' );
 add_action( 'after_setup_theme', function () {
     load_child_theme_textdomain( 'corpobox', get_stylesheet_directory() . '/languages' );
 } );
+
+
+function fff_hero( $atts ) {
+	$atts = shortcode_atts( array(
+		'title'       => 'no foo',
+        'button_href' => '#',
+        'button_text'  => 'Klickense hier!'
+    ), $atts, 'fff_hero' );
+
+    $o = '<section id="prebefore-home-widget">';
+    $o .= '<div class="widget">';
+    $o .= '<div class="call-to-action">';
+
+    $o .= '<div class="call-to-action-text">';
+    $o .= '<h3>' . $atts['title'] . '</h3>';
+    $o .= '</div><!-- .call-to-action-text -->';
+
+    $o .= '<div class="call-to-action-button">';
+    $o .= '<a href="' . $atts['button_href'] . '" class="btn fff-button">' . $atts['button_text'] . '</a>';
+    $o .= '</div><!-- .call-to-action-button -->';
+
+    $o .= '<div class="clearfix"></div>';
+    $o .= '</div><!-- .call-to-action -->';
+    $o .= '</div><!-- .widget -->';
+    $o .= '</section>';
+
+	return $o;
+}
+
+add_shortcode( 'fff_hero', 'fff_hero' );
 function fff_icon_widget( $atts ) {
     $atts = shortcode_atts( array(
         'icon'  => 'fa-wordpress',
